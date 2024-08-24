16e065b678c56dc0528f9c1fe458b42460b4a601
P J THARUN <pjtharun01@gmail.com>
2024-08-24 10:54:54 +0530

    Project Intialized

---
+++ src/main/java/com/ets/EducationTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationTrackingSystemApplication.class, args);
+	}
+
+}
